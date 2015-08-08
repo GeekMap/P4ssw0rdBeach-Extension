487e2d7b4974a34ab6ceb145418bb671d98fd47a
Chuck Lin <chuck42315@gmail.com>
2015-08-08 14:21:30 +0800

    fix open multiple windows problem

--- Chrome/background.js
+++ Chrome/background.js
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 var SERVER_URL = "https://api.parse.com/1/functions/checkSite";
+var bloglink = null;
 
 function examinURL(url) {
     if (url.lastIndexOf("chrome://", 0) !== 0) {
@@ -37,13 +38,17 @@ function showNotification(siteUrl, link) {
         iconUrl: "icon.png",
         buttons: [{"title": "Go to blog page"}],
     };
-    chrome.notifications.create(null, opt, function(id) {});
-    chrome.notifications.onButtonClicked.addListener(function(notificationId, buttonIndex) {
-        if (buttonIndex === 0) {
-            window.open(link);
-        }
+    chrome.notifications.create("nudepwd", opt, function(notificationId) {
+        bloglink = link; 
     });
 }
+
+chrome.notifications.onButtonClicked.addListener(function(notificationId, buttonIndex) {
+    if (notificationId == "nudepwd" && buttonIndex === 0) {
+        window.open(bloglink);
+        chrome.notifications.clear("nudepwd");
+    }
+});
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
     if (changeInfo.status === 'complete') {
